b0882c2201f171f68038e4d67a4ae0d62bcad61d
omarkhaled <okoros192000@gmail.com>
2023-05-16 02:45:34 +0300

    update my project

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let btn = document.getElementById("btn-menu");
 btn.addEventListener("click", () => {
   let linksMenu = document.querySelector("#sec-1 nav .links");
-  if ((linksMenu.style.display != "block"))
-      linksMenu.style.display = "block";
+  if ((linksMenu.style.display != "flex"))
+      linksMenu.style.display = "flex";
     else {
       linksMenu.style.display = "none";
     }
